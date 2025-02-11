c372f764265e41cdf39e3e8f9c70a6d1bec8194b
Syfrim <ssyfrim@gmail.com>
2025-02-11 13:06:26 +0800

    Nav and hero aspect minmially completed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,15 @@
 import StoreLinks, { BtnTypes } from "../common/StoreLinks";
-import Phone from "../assets/mockup.svg";
+import Piano2 from "../assets/file.svg";
 import HeroImage from "../assets/hero.png";
+import Piano1 from "../assets/piano1.jpg";
 
 function Hero() {
     return( 
         <section className="relative items-center gap-10 px-8 sm:gap-16 md:my-0 md:h-[100vh] mt-4 flex h-fit max-w-7xl flex-col md:flex-row md:gap-0 lg:px-12 xl:m-auto xl:gap-0 xl:overflow-hidden">
             <div className="sm:w-full md:w-3/6">
-                <h1 className="mx-auto mb-8 w-[12ch] text-center text-4xl font-semibold text-gray-800 sm:text-5xl md:mx-0 md:text-left">Start spending the smart way</h1>
+                <h1 className="mx-auto mb-8 w-[12ch] text-center text-4xl font-semibold text-gray-800 sm:text-5xl md:mx-0 md:text-left">Where every note finds it's perfect match</h1>
                 <p className="m-auto w-[34ch] text-center text-gray-500 md:m-0 md:text-left">
-                    Take Control of Your Finances Anytime, Anywhere with Kobodrop, Discover the Smart Way to Use Your Money!
+                    Finding the right piano teacher shouldn't be a struglle, it should feel like the start of a beautiful melody.
                 </p>
 
                 <StoreLinks type={BtnTypes.Standard}/>
@@ -16,8 +17,8 @@ function Hero() {
             </div>
            
             <div className="md:w-3/6 xl:mb-12 xl:overflow-hidden">
-                <img className="right-0 m-auto w-72 xl:absolute xl:left-6 xl:right-0 xl:mt-32 xl:w-80" src={Phone} alt="Kobodrop app frame" />
-                <img className="hidden rounded-2xl xl:flex " src={HeroImage} alt="A woman happily using Kobodrop" />
+                <img className="right-0 m-auto w-72 xl:absolute xl:left-6 xl:right-0 xl:mt-32 xl:w-80" src={Piano2} alt="Kobodrop app frame" />
+                <img className="hidden rounded-2xl xl:flex " src={Piano1} alt="A woman happily using Kobodrop" />
             </div>
 
 
--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -3,6 +3,7 @@ import { Disclosure, DisclosureButton } from '@headlessui/react';
 import  Kobodrop  from "../assets/logos/kobodrop_logo.svg";
 import ButtonLink from '../common/ButtonLink';
 import { Children } from 'react';
+import NotebyNoteLogo from "../assets/NotebyNoteLogo_sized.png"
 
 function Nav() {
     const textLinkClasses = "text-gray-500 hover:text-gray-900 active:text-gray-400";
@@ -20,7 +21,7 @@ function Nav() {
           <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8 xlm:-auto lg:px-12">   
             <div className="flex">
              
-                <ButtonLink href="#" children={<img src={Kobodrop} alt="Kobodrop logo"/>}/>                
+                <ButtonLink href="#" children={<img src={NotebyNoteLogo} alt="Kobodrop logo"/>}/>                
                 <div className='sm:flex hidden items-center space-x-4 ml-4 lg:ml-8 lg:space-x-8'>
 
                     {navLinks.map((link, index) => (
@@ -30,7 +31,7 @@ function Nav() {
 
                 </div>
             </div>
-            <ButtonLink href="https://play.google.com" target="_blank" children={"Install Kobodrop"} className='active: bg hidden bg-gray-800 text-white rounded-xl px-5 py-3 hover:bg-gray-900 active:bg-gray-600 sm:flex'></ButtonLink>
+            <ButtonLink href="https://play.google.com" target="_blank" children={"Contact Us"} className='active: bg hidden bg-gray-800 text-white rounded-xl px-5 py-3 hover:bg-gray-900 active:bg-gray-600 sm:flex'></ButtonLink>
             
             <DisclosureButton className="sm:hidden rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white">
                 {open ? (
