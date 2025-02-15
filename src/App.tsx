514c2fd4164f61b407bf18e2c717e80a87e0d953
Syfrim <ssyfrim@gmail.com>
2025-02-16 00:14:25 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <Footer />
         <button className="fixed bottom-6 right-8 size-10 z-50" onClick={() => window.open("https://wa.me/6596527576?text=Hello%2C%20I%E2%80%99m%20looking%20for%20a%20piano%20teacher%20for%20my%20child.%20Could%20you%20help%20me%20find%20the%20right%20match%3F", "_blank")} >
         <img 
-          src="../src/assets/whatsapp-svgrepo-com (1).png" 
+          src="/src/assets/whatsapp-svgrepo-com (1).png" 
           alt="WhatsApp" 
           className="w-full h-full"
         />
