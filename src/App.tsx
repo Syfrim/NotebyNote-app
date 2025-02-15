61573ffb132196c3e5d1f30a5a1c7035233a0762
Syfrim <ssyfrim@gmail.com>
2025-02-16 00:18:40 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Features from "./sections/Features";
 import Reviews from "./sections/Reviews";
 import Footer from "./sections/Footer";
 import Choice from "./sections/Choice";
+import WhatsappIcon from "../src/assets/whatsapp-svgrepo-com (1).png"
 
 function App() {
   return(
@@ -16,7 +17,7 @@ function App() {
         <Footer />
         <button className="fixed bottom-6 right-8 size-10 z-50" onClick={() => window.open("https://wa.me/6596527576?text=Hello%2C%20I%E2%80%99m%20looking%20for%20a%20piano%20teacher%20for%20my%20child.%20Could%20you%20help%20me%20find%20the%20right%20match%3F", "_blank")} >
         <img 
-          src="/src/assets/whatsapp-svgrepo-com (1).png" 
+          src={WhatsappIcon}
           alt="WhatsApp" 
           className="w-full h-full"
         />
