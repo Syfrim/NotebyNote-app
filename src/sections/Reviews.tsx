449e944cfd39566c8c3e6f45511d148888dada14
Syfrim <ssyfrim@gmail.com>
2025-02-18 12:01:21 +0800

    testimonial change

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -4,23 +4,23 @@ function Reviews() {
     const slides = [
         {
             src : "https://images.unsplash.com/photo-1561014816-50ce5acb5e73?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            text: '"Ever since I’ve been learning piano here, I made great improvements! The lessons are engaging, and my teacher always encourages me to do my best. I can now play more advanced pieces, and I feel more confident performing!"',
-            name: "Ryan Lim",
-            age: "13 Years Old, Grade 4"
+            text: '“I was looking for a piano teacher for my son, and this platform made the search so convenient. Within days, we found a patient and experienced instructor who comes to our home. My son enjoys his lessons, and we’re seeing great progress!”',
+            name: "Jason Lim",
+            age: "40 Years Old, Parent of a Young Learner"
         }
         ,
         {
             src: "https://cdn.pixabay.com/photo/2019/06/24/00/59/piano-4294992_1280.jpg",
-            text: '"I love my piano lessons! My teacher is so patient and makes learning really fun. I was nervous at first, but now I can play songs all by myself. I always look forward to my classes!"',
-            name: "Emily Tan",
-            age: "9 Years Old, Grade 1"
+            text: '“Finding a reliable and patient piano teacher for my kids used to be stressful, but this platform made it effortless. The matching process was smooth, and the teacher is experienced and engaging. Highly recommended!”',
+            name: "Sophia Lee",
+            age: "45 Years Old, Mother of Two"
         }
         ,
         {
             src: "https://cdn.pixabay.com/photo/2019/03/27/00/21/keyboard-4083960_1280.jpg",
-            text: '"The piano tuition has really helped me refine my technique and musical expression. My teacher gives great feedback and always challenges me to grow. Thanks to these lessons, I recently passed my Grade 8 exam with distinction!"',
-            name: "Sofia Ng",
-            age: "17 Years Old, Grade 8"
+            text: '“I’ve tried different ways to learn piano, but this is by far the best. The platform made it so easy to find a skilled teacher who fits me”',
+            name: "Michelle Ng",
+            age: "50 Years Old, Music Enthusiast"
         }
     ];
   return (
